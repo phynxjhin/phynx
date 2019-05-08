bdd84ec05ef53e855f5ddc660ec47756edb1203d
phynxjhin <49936261+phynxjhin@users.noreply.github.com>
2019-05-08 14:52:09 +0700

    Revert "web"
    This reverts commit 7cca98fc422624af04db9db3322eea873110a266.

--- MySql.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_name = "MyDB";
-$db_username  = "root";
-$db_password = "";
-@mysql_connect("{$db_host}", "{db_username}", "{$db_password}") or die("Can't connect to database");
-@mysql_select_db("{$db_name}") or die("Can't connect to database");
-?>
